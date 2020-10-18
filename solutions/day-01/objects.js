41e6d69241020892a732d51a0438b4e3cedeae6a
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2020-10-17 23:46:58 -0700

    Objects Exercise: Level 3

--- solutions/day-01/objects.js
+++ solutions/day-01/objects.js
@@ -173,4 +173,58 @@ console.log(keys);
 const values = Object.values(users);
 console.log(values);
 
-// 7. Countries
+// Objects Exercise - Level 3
+
+const mongoUsers = [
+  {
+    _id: "ab12ex",
+    username: "Alex",
+    email: "alex@alex.com",
+    password: "123123",
+    createdAt: "08/01/2020 9:00 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "fg12cy",
+    username: "Asab",
+    email: "asab@asab.com",
+    password: "123456",
+    createdAt: "08/01/2020 9:30 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "zwf8md",
+    username: "Brook",
+    email: "brook@brook.com",
+    password: "123111",
+    createdAt: "08/01/2020 9:45 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "eefamr",
+    username: "Martha",
+    email: "martha@martha.com",
+    password: "123222",
+    createdAt: "08/01/2020 9:50 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "ghderc",
+    username: "Thomas",
+    email: "thomas@thomas.com",
+    password: "123333",
+    createdAt: "08/01/2020 10:00 AM",
+    isLoggedIn: false,
+  },
+];
+
+let signUp = (username) => {
+  if (mongoUsers.some((x) => x.username === username)) {
+    return "You already have an account";
+  } else {
+    return "Sign Up to create an acount";
+  }
+};
+
+console.log(signUp("Alex"));
+console.log(signUp("Chrys"));
